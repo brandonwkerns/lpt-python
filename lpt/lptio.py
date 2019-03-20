2f597da2477986ebfd9770e308cfa5f435430d33
Brandon Kerns <bkerns@uw.edu>
2019-03-20 11:23:47 -0700

    Start function for pixel LP object output.

--- lpt/lptio.py
+++ lpt/lptio.py
@@ -6,13 +6,12 @@ import numpy as np
 ### Output functions
 ###################################################
 def lp_objects_output_ascii(fn, OBJ):
-
+    """
+    This function outputs the "bulk" LP object properties (centroid, date, area)
+    to an ascii file.
+    """
     print('Writing ascii output to: '+fn)
-
-    #fmt = '%7.2f%8.2f%7.1f%7.1f%20.1f   %04d%02d%02d%02d      %7.2f%7.2f%7.1f  %7.2f\n' ;
-    #fmt = '%7.2f%8.2f%7.1f%7.1f%20.1f   %04d%02d%02d%02d\n'
     fmt = '%7.2f%8.2f%7.1f%7.1f%20.1f   %16d\n'
-
     file = open(fn, 'w')
 
     for ii in range(len(OBJ['lon'])):
@@ -28,5 +27,9 @@ def lp_objects_output_ascii(fn, OBJ):
     file.close()
 
 
-def lp_objects_output_netcdf():
+def lp_objects_output_netcdf(fn, OBJ, label_im):
     pass
+    """
+    This function outputs the "bulk" LP object properties (centroid, date, area)
+    Plus the pixel information to a compressed netcdf file.
+    """
