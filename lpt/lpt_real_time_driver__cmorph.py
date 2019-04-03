bb9adb8a39be60ad6c065cd1be8982348cb57e94
Brandon Kerns <bkerns@uw.edu>
2019-04-03 15:17:29 -0700

    Fix memory issue with multiple figure windows.

--- lpt/lpt_real_time_driver__cmorph.py
+++ lpt/lpt_real_time_driver__cmorph.py
@@ -3,11 +3,13 @@ import numpy as np
 from context import lpt
 import matplotlib.pylab as plt
 import datetime as dt
+import sys
 import os
 import matplotlib.colors as colors
 import scipy.ndimage
 
 plt.close('all')
+plt.ioff()
 
 """
 Main settings for lpt
@@ -53,13 +55,26 @@ def cmap_map(function, cmap):
 
     return colors.LinearSegmentedColormap('colormap',cdict,1024)
 
-year, month, day, hour = dt.datetime.utcnow().timetuple()[0:4]
-hour = int(np.floor(hour/3) * 3)
+## Use current real time, or specified time from the command line args.
+if len(sys.argv) < 2:
+    base_time = dt.datetime.utcnow()
+else:
+    base_time = dt.datetime.strptime(str(sys.argv[1]), '%Y%m%d%H') # command line arg #1 format: YYYYMMDDHH
+
+if len(sys.argv) < 3:
+    hours_to_go_back = 12
+else:
+    hours_to_go_back = int(sys.argv[2]) # command line arg #2: hours to go back.
+
+year, month, day, hour = base_time.timetuple()[0:4]
+hour = int(np.floor(hour))
 
 current_end_of_accumulation_time = dt.datetime(year,month,day,hour,0,0)
 
+fig = plt.figure(figsize=(8.5,4))
+
 ## Check back 12 h from current time.
-for hours_back in range(0, 13, data_time_interval):
+for hours_back in range(0, hours_to_go_back+1, data_time_interval):
 
     try:
         end_of_accumulation_time = current_end_of_accumulation_time - dt.timedelta(hours=hours_back)
@@ -75,12 +90,16 @@ for hours_back in range(0, 13, data_time_interval):
 
         ## Get accumulated rain.
         data_collect = []
+        count = 0
         for this_dt in reversed(dt_list):
             DATA_RAW=lpt.readdata.read_cmorph_at_datetime(this_dt, area=[40,210,-40,40], verbose=True)
-            data_collect.append(DATA_RAW['precip'][0,:,:])
-            data_collect.append(DATA_RAW['precip'][1,:,:])
-        data_collect3d = np.array(data_collect)
-        DATA_ACCUM = np.nanmean(data_collect3d,axis=0) * 24.0
+            if count < 1:
+                data_collect = np.array(0.5*(DATA_RAW['precip'][0,:,:] + DATA_RAW['precip'][1,:,:]))
+            else:
+                data_collect += np.array(0.5*(DATA_RAW['precip'][0,:,:] + DATA_RAW['precip'][1,:,:]))
+            count += 1
+
+        DATA_ACCUM = (data_collect/count) * 24.0 # Get the mean in mm/day.
         print('Accum done.',flush=True)
 
         ## Filter the data
@@ -105,11 +124,11 @@ for hours_back in range(0, 13, data_time_interval):
          + str(end_of_accumulation_time.day).zfill(2)
          + str(end_of_accumulation_time.hour).zfill(2))
         lpt.lptio.lp_objects_output_ascii(objects_fn, OBJ)
-        lpt.lptio.lp_objects_output_netcdf(objects_fn + '.nc', OBJ)
+        if (len(OBJ['n_points']) > 0):
+            lpt.lptio.lp_objects_output_netcdf(objects_fn + '.nc', OBJ)
 
         ## Plot
-        fig = plt.figure(figsize=(8.5,4))
-
+        plt.clf()
         ax1 = fig.add_subplot(111)
         map1=lpt.helpers.plot_map_background(plot_area)
         cmap = cmap_map(lambda x: x/2 + 0.5, plt.cm.jet)
@@ -129,6 +148,7 @@ for hours_back in range(0, 13, data_time_interval):
         file_out_base = (img_dir2 + '/lp_objects_cmorph_rt_' + YMDH)
 
         lpt.helpers.print_and_save(file_out_base)
+        plt.clf()
 
     except FileNotFoundError:
         print('Data not yet available up to this point. Skipping.')
