7480b48bb61de2d7013d69d2ef1eebec94ab883c
Brandon Kerns <bkerns@uw.edu>
2019-05-30 16:56:06 -0700

    Related to splitting/merging. (Not ready yet).

--- tests/test_tracking_branches.py
+++ tests/test_tracking_branches.py
@@ -11,12 +11,15 @@ options={}
 options['min_overlap_points'] = 4000
 options['min_overlap_frac'] = 0.5
 options['min_lp_objects_points'] = 100
+options['center_jump_max_hours'] = 3*24   # How long to allow center jumps
+options['min_lpt_duration_hours'] = 7*24  # Minumum duration to keep it as an LPT
 
 
 ## For TMPA
 options['objdir'] = '/home/orca/bkerns/public_html/realtime_mjo_tracking/lpt/data/tmpa/objects'
 #dt_list = [dt.datetime(2019,2,1,0,0,0) + dt.timedelta(hours=x) for x in np.arange(0.0,61*24.0+1,3.0)]
-dt_list = [dt.datetime(2019,3,1,0,0,0) + dt.timedelta(hours=x) for x in np.arange(0.0,15*24.0+1,3.0)]
+dt_list = [dt.datetime(2019,3,1,0,0,0) + dt.timedelta(hours=x) for x in np.arange(0.0,16*24.0+1,3.0)]
+#dt_list = [dt.datetime(2019,3,1,0,0,0) + dt.timedelta(hours=x) for x in np.arange(0.0,4*24.0+1,3.0)]
 
 
 ## For CMORPH
@@ -55,18 +58,34 @@ lpt.helpers.plot_lpt_groups_time_lon_text(ax1, LPTb, options)
 ax1.set_title('Backwards')
 
 print('Merging forward and backwards')
-print(dt.datetime.now(), flush=True)
-LPTfb = lpt.helpers.overlap_forward_backward(LPTf.copy(), LPTb.copy(), options, verbose=True)
-print(dt.datetime.now(), flush=True)
+LPTfb0 = lpt.helpers.overlap_forward_backward(LPTf.copy(), LPTb.copy(), options, verbose=True)
 
+## Allow center jumps.
+print(('Allow center jumps up to ' + str(options['center_jump_max_hours']) + ' hours.'))
+LPTfb1 = lpt.helpers.lpt_group_array_allow_center_jumps(LPTfb0, options)
+
+print(('Remove LPT shorter than ' + str(options['min_lpt_duration_hours']) + ' hours.'))
+LPTfb2 = lpt.helpers.remove_short_lived_systems(LPTfb1, options['min_lpt_duration_hours'])
+
+LPTfb = lpt.helpers.lpt_group_id_separate_branches(LPTfb2, options, verbose=True)
 
 print('Calc LP system group bulk properties.')
-print(dt.datetime.now(), flush=True)
-TIMECLUSTERS0 = lpt.helpers.calc_lpt_system_group_properties(LPTfb, options)
-print(dt.datetime.now(), flush=True)
+TIMECLUSTERS0 = lpt.helpers.calc_lpt_system_group_properties_with_branches(LPTfb, options)
 
-#print('Calc LP system group bulk properties with branches.')
-#LPTbranches = lpt.helpers.separate_lpt_system_branches(LPTfb, LPTf, LPTb, options)
+
+## Re-combine when splits/mergers are < 3 days.
+lpt_diff = lpt.helpers.lpt_branches_difference(LPTfb, 1, 1, 2)
+print('Diff')
+print(str(lpt_diff))
+print(str(LPTfb[lpt_diff,1].astype('int')))
+
+lpt_intersection = lpt.helpers.lpt_branches_intersection(LPTfb, 1, 1, 2)
+print('Intersection')
+print(str(lpt_intersection))
+print(str(LPTfb[lpt_intersection,1].astype('int')))
+
+intersection_timestamp_begin =  LPTfb[lpt_intersection[0],0]
+intersection_timestamp_end =  LPTfb[lpt_intersection[-1],0]
 
 #fig = plt.figure(figsize=(8.5,11))
 ax = fig0.add_subplot(133)
@@ -81,5 +100,7 @@ plt.savefig('test_tracking.png')
 
 fn_tc = './TEST.txt'
 lpt.lptio.lpt_system_tracks_output_ascii(fn_tc, TIMECLUSTERS0)
+
+
 
 #plt.show()
