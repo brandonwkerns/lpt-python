5cb6d8f0c9165f1906d75c1889966d7334aecb41
Brandon Kerns <bkerns@uw.edu>
2019-08-09 11:38:26 -0700

    Add rain colors and post process plotting functions.

--- lpt/plotting.py
+++ lpt/plotting.py
@@ -11,12 +11,44 @@ import matplotlib.colors as colors
 from mpl_toolkits.basemap import Basemap
 from matplotlib.colors import ListedColormap
 import matplotlib.colors as colors
+from netCDF4 import Dataset
 
 """
 This module file contains function for making LPT related plots.
 Most of the plotting function take a Matplotlib axis object as input
 So create Matlplotlib figures and axes before calling the functions.
 """
+
+
+
+
+
+
+def rain_cmap_12():
+
+    cmap = plt.cm.jet
+    N_keep = 12
+    cmap2 = colors.LinearSegmentedColormap.from_list(name='custom', colors=cmap(range(cmap.N)), N=N_keep)
+    color_list1 = cmap2(range(cmap2.N))
+    ## Modify individual colors here.
+    color_list11 = cmap2(range(cmap2.N))
+    color_list11[0] = [1,1,1,1] # Make first color white.
+    color_list11[1] = [0.7,0.7,0.7, 1] # Make first color gray.
+    color_list11[2] = [0.0, 0.9, 1.0, 1.0]
+    color_list11[3] = [0.0/255.0, 156.0/255.0, 255.0/255.0, 1.0]
+    color_list11[4] = [0.0/255.0, 58.0/255.0, 255.0/255.0, 1.0]
+    color_list11[5] = [0.3, 1.0, 0.3, 1.]
+    color_list11[6] = [0.0, 0.8, 0.0, 1.]
+    color_list11[9] = color_list1[10]  # Move top to colors of jet color map down.
+    color_list11[10] = color_list1[11] # Move top to colors of jet color map down.
+    color_list11[11] = [1,0,1,1] # Magenta on top.
+
+    color_list11[:,-1] = np.linspace(0.25, 1, N_keep)
+
+    cmap = colors.LinearSegmentedColormap.from_list(name='custom', colors=color_list11, N=N_keep)
+
+    return cmap
+
 
 def cmap_map(function, cmap):
     """ Applies function (which should operate on vectors of shape 3: [r, g, b]), on colormap cmap.
@@ -188,3 +220,84 @@ def plot_timelon_with_lpt(ax2, dt_list, lon, timelon_rain, TIMECLUSTERS
     plt.yticks(fontsize=label_font_size)
 
     return (Hrain)
+
+################################################################################
+###### Functions used for post processing scripts. #############################
+################################################################################
+
+def manage_time_lon(year1, parent_dir='.'):
+
+    fn_timelon = (parent_dir + '/time_lon_rain_15S_15N__' + str(year1) + '_' + str(year1+1) + '.nc')
+    DATA={}
+
+    if os.path.isfile(fn_timelon):
+        print('Reading: ', fn_timelon)
+        DS = Dataset(fn_timelon, 'r')
+        DATA['lon'] = DS['lon'][:]
+        DATA['time'] = [dt.datetime(1970,1,1,0,0,0) + dt.timedelta(hours=int(x)) for x in DS['time'][:]]
+        DATA['time3'] = [dt.datetime(1970,1,1,0,0,0) + dt.timedelta(hours=int(x)) for x in DS['time3'][:]]
+        DATA['precip'] = DS['precip'][:]
+        DATA['precip3'] = DS['precip3'][:]
+        DS.close()
+
+    else:
+
+        begin_time = dt.datetime(year1,6,1,0,0,0)
+        end_time = dt.datetime(year1+1,6,1,0,0,0)
+
+        hours_list = np.arange(0.0, 0.1 +(end_time-begin_time).total_seconds()/3600.0, interval_hours)
+        dt_list = [begin_time + dt.timedelta(hours=x) for x in hours_list]
+
+        timelon_rain = []
+        for this_dt in dt_list:
+            DATA_RAW = read_function(this_dt, verbose=True)
+
+            lat_idx, = np.where(np.logical_and(DATA_RAW['lat'] > -15.0, DATA_RAW['lat'] < 15.0))
+            timelon_rain.append(np.mean(np.array(DATA_RAW['precip'][lat_idx,:]), axis=0))
+
+        lon = DATA_RAW['lon']
+        timelon_rain = np.array(timelon_rain)
+        dt_list3 = dt_list[0::int(accumulation_hours/interval_hours)]
+        timelon_rain3 = []
+        for tt in range(0,len(dt_list),int(accumulation_hours/interval_hours)):
+            timelon_rain3.append(np.nanmean(timelon_rain[tt:tt+int(accumulation_hours/interval_hours),:], axis=0))
+        timelon_rain3 = np.array(timelon_rain3)
+
+        print('Writing: ', fn_timelon)
+        os.mkdirs(parent_dir, exist_ok=True)
+        DS = Dataset(fn_timelon, 'w')
+
+        DS.createDimension('lon', len(DATA_RAW['lon']))
+        DS.createDimension('time', len(dt_list))
+        DS.createDimension('time3', len(dt_list3))
+
+        DS.createVariable('lon','d',('lon',))
+        DS.createVariable('time','d',('time',))
+        DS.createVariable('time3','d',('time3',))
+        DS.createVariable('precip','d',('time','lon',))
+        DS.createVariable('precip3','d',('time3','lon',))
+
+        DS['lon'][:] = DATA_RAW['lon']
+        DS['time'][:] = [(x - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0 for x in dt_list]
+        DS['time3'][:] = [(x - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0 for x in dt_list3]
+        DS['precip'][:] = timelon_rain
+        DS['precip3'][:] = timelon_rain3
+
+        DATA['lon'] = DATA_RAW['lon']
+        DATA['time'] = [dt.datetime(1970,1,1,0,0,0) + dt.timedelta(hours=int(x)) for x in dt_list]
+        DATA['time3'] = [dt.datetime(1970,1,1,0,0,0) + dt.timedelta(hours=int(x)) for x in dt_list3]
+        DATA['precip'] = timelon_rain
+        DATA['precip3'] = timelon_rain3
+
+        DS.close()
+
+    ## Data return
+    return DATA
+
+
+def print_and_save(file_out_base, pdf=False):
+    if pdf:
+        print(file_out_base + '.pdf')
+        plt.savefig(file_out_base + '.pdf' ,bbox_inches='tight', dpi=150)
+    print(file_out_base + '.png')
+    plt.savefig(file_out_base + '.png' ,bbox_inches='tight', dpi=150)
