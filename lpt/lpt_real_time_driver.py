bdad45e1b4797465f454679b4e8ae2778139541d
Brandon Kerns <bkerns@uw.edu>
2019-08-26 17:48:16 -0700

    Check if LPO step already done. Use new tracking function.

--- lpt/lpt_real_time_driver.py
+++ lpt/lpt_real_time_driver.py
@@ -8,7 +8,8 @@ import os
 import matplotlib.colors as colors
 import scipy.ndimage
 
-## This driver script is for analysis or data, e.g., going backward in time.
+## This driver script is for real time analysis or data
+##   It is for going backwards from a specified time (e.g., near real time).
 
 def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_split_options,argv):
 
@@ -31,7 +32,8 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
         fig1 = plt.figure(1, figsize = (8.5,4))
         fig2 = plt.figure(2, figsize = (8.5,11))
 
-    ## Check back 24 h from current time.
+
+    ## Check back "hours_to_go_back" h from current time.
     for hours_back in range(0, hours_to_go_back+1, dataset['data_time_interval']):
 
         try:
@@ -42,72 +44,86 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
 
             beginning_of_accumulation_time = end_of_accumulation_time - dt.timedelta(hours=lpo_options['accumulation_hours'])
             print((beginning_of_accumulation_time, end_of_accumulation_time))
-            dt_list = [beginning_of_accumulation_time
-                + dt.timedelta(hours=x) for x in np.double(np.arange(0,lpo_options['accumulation_hours']
-                                                  + dataset['data_time_interval'],dataset['data_time_interval']))]
 
-            ## Get accumulated rain.
-            data_collect = []
-            count = 0
+            if lpo_options['do_lpo_calc']:
 
-            for this_dt in reversed(dt_list):
-                if 'sub_area' in dataset.keys():
-                    DATA_RAW = dataset['read_function'](this_dt, verbose=dataset['verbose'], area=dataset['sub_area'])
-                else:
-                    DATA_RAW = dataset['read_function'](this_dt, verbose=dataset['verbose'])
-                if count < 1:
-                    data_collect = np.array(DATA_RAW['precip'])
-                else:
-                    data_collect += np.array(DATA_RAW['precip'])
-                count += 1
 
-            DATA_ACCUM = (data_collect/count) * 24.0 # Get the mean in mm/day.
-            print('Accum done.',flush=True)
+                """
+                Set Object Output files
+                """
+                objects_dir = (output['data_dir'] + '/' + dataset['label'] + '/objects/'
+                                + end_of_accumulation_time.strftime(output['sub_directory_format']))
+                os.makedirs(objects_dir, exist_ok = True)
+                objects_fn = (objects_dir + '/objects_' + YMDH)
 
-            ## Filter the data
-            DATA_FILTERED = scipy.ndimage.gaussian_filter(DATA_ACCUM, lpo_options['filter_stdev']
-                , order=0, output=None, mode='reflect', cval=0.0, truncate=3.0)
-            print('filter done.',flush=True)
+                if os.path.exists(objects_fn):
+                    print('This time already has LPO step done.')
+                    continue
 
-            ## Get LP objects.
-            label_im = lpt.helpers.identify_lp_objects(DATA_FILTERED, lpo_options['thresh'], verbose=dataset['verbose'])
-            OBJ = lpt.helpers.calculate_lp_object_properties(DATA_RAW['lon'], DATA_RAW['lat']
-                        , DATA_RAW['precip'], DATA_ACCUM, DATA_FILTERED, label_im, 0
-                        , end_of_accumulation_time, verbose=True)
-            print('objects properties.',flush=True)
+                """
+                Get accumulated rain.
+                """
 
-            """
-            Object Output files
-            """
-            objects_dir = (output['data_dir'] + '/' + dataset['label'] + '/objects/'
-                            + end_of_accumulation_time.strftime(output['sub_directory_format']))
-            os.makedirs(objects_dir, exist_ok = True)
-            objects_fn = (objects_dir + '/objects_' + YMDH)
-            lpt.lptio.lp_objects_output_ascii(objects_fn, OBJ)
-            if (len(OBJ['n_points']) > 0):
+                dt_list = [beginning_of_accumulation_time
+                    + dt.timedelta(hours=x) for x in np.double(np.arange(0,lpo_options['accumulation_hours']
+                                                      + dataset['data_time_interval'],dataset['data_time_interval']))]
+
+                data_collect = []
+                count = 0
+
+                for this_dt in reversed(dt_list):
+                    if 'sub_area' in dataset.keys():
+                        DATA_RAW = dataset['read_function'](this_dt, verbose=dataset['verbose'], area=dataset['sub_area'])
+                    else:
+                        DATA_RAW = dataset['read_function'](this_dt, verbose=dataset['verbose'])
+                    if count < 1:
+                        data_collect = np.array(DATA_RAW['precip'])
+                    else:
+                        data_collect += np.array(DATA_RAW['precip'])
+                    count += 1
+
+                DATA_ACCUM = (data_collect/count) * 24.0 # Get the mean in mm/day.
+                print('Accum done.',flush=True)
+
+                ## Filter the data
+                DATA_FILTERED = scipy.ndimage.gaussian_filter(DATA_ACCUM, lpo_options['filter_stdev']
+                    , order=0, output=None, mode='reflect', cval=0.0, truncate=3.0)
+                print('filter done.',flush=True)
+
+                ## Get LP objects.
+                label_im = lpt.helpers.identify_lp_objects(DATA_FILTERED, lpo_options['thresh'], verbose=dataset['verbose'])
+                OBJ = lpt.helpers.calculate_lp_object_properties(DATA_RAW['lon'], DATA_RAW['lat']
+                            , DATA_RAW['precip'], DATA_ACCUM, DATA_FILTERED, label_im, 0
+                            , end_of_accumulation_time, verbose=True)
+                print('objects properties.',flush=True)
+
+                """
+                Do Object Files output
+                """
+                lpt.lptio.lp_objects_output_ascii(objects_fn, OBJ)
                 lpt.lptio.lp_objects_output_netcdf(objects_fn + '.nc', OBJ)
 
-            """
-            Object Plot
-            """
-            if plotting['do_plotting']:
-                plt.figure(1)
-                fig1.clf()
-                ax1 = fig1.add_subplot(111)
-                lpt.plotting.plot_rain_map_with_filtered_contour(ax1
-                        , DATA_ACCUM, OBJ
-                        , plot_area = plotting['plot_area'])
-                ax1.set_title((dataset['label'].upper() + ' RT '
-                                + str(lpo_options['accumulation_hours'])
-                                + '-h Rain Rate and LP Objects\nEnding ' + YMDH_fancy))
+                """
+                Object Plot
+                """
+                if plotting['do_plotting']:
+                    plt.figure(1)
+                    fig1.clf()
+                    ax1 = fig1.add_subplot(111)
+                    lpt.plotting.plot_rain_map_with_filtered_contour(ax1
+                            , DATA_ACCUM, OBJ
+                            , plot_area = plotting['plot_area'])
+                    ax1.set_title((dataset['label'].upper() + ' RT '
+                                    + str(lpo_options['accumulation_hours'])
+                                    + '-h Rain Rate and LP Objects\nEnding ' + YMDH_fancy))
 
-                img_dir1 = (output['img_dir'] + '/' + dataset['label'] + '/objects/'
-                                + end_of_accumulation_time.strftime(output['sub_directory_format']))
+                    img_dir1 = (output['img_dir'] + '/' + dataset['label'] + '/objects/'
+                                    + end_of_accumulation_time.strftime(output['sub_directory_format']))
 
-                os.makedirs(img_dir1, exist_ok = True)
-                file_out_base = (img_dir1 + '/lp_objects_' + dataset['label'] + '_rt_' + YMDH)
-                lpt.plotting.print_and_save(file_out_base)
-                fig1.clf()
+                    os.makedirs(img_dir1, exist_ok = True)
+                    file_out_base = (img_dir1 + '/lp_objects_' + dataset['label'] + '_rt_' + YMDH)
+                    lpt.plotting.print_and_save(file_out_base)
+                    fig1.clf()
 
         except FileNotFoundError:
             print('Data not yet available up to this point. Skipping.')
@@ -145,13 +161,9 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
         LPT0, BRANCHES0 = lpt.helpers.lpt_group_array_remove_small_objects(LPT0, BRANCHES0, options)
 
         ## Connect forward, then backwards
-        print('Forward...')
-        LPTf, BRANCHESf = lpt.helpers.calc_lpt_group_array(LPT0, BRANCHES0, options, verbose=True)
-        print('Backward...')
-        LPTb, BRANCHESb = lpt.helpers.calc_lpt_group_array(LPT0, BRANCHES0, options, verbose=True, reversed=True)
-
-        ## Overlap forward and backward connections.
-        LPTfb, BRANCHESfb = lpt.helpers.overlap_forward_backward(LPTf, LPTb, BRANCHESf, BRANCHESb, options, verbose=True)
+        print('Connecting objects...')
+        LPTfb, BRANCHESfb = lpt.helpers.calc_lpt_group_array2(LPT0, BRANCHES0, options, min_points = lpt_options['min_lp_objects_points'], verbose=True)
+        lpt.lptio.lpt_systems_group_array_output_ascii('test0.txt', LPTfb, BRANCHESfb)
 
         ## Allow center jumps.
         print(('Allow center jumps up to ' + str(options['center_jump_max_hours']) + ' hours.'))
@@ -159,19 +171,22 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
 
         ## Eliminate short duration systems.
         print(('Remove LPT shorter than ' + str(options['min_lpt_duration_hours']) + ' hours.'))
-        LPT_remove_short, BRANCHES_remove_short = lpt.helpers.remove_short_lived_systems(LPT_center_jumps, BRANCHESfb, options['min_lpt_duration_hours']
-                                , latest_datetime = latest_lp_object_time - dt.timedelta(hours=options['min_lpt_duration_hours']))
+        LPT_remove_short, BRANCHES_remove_short = lpt.helpers.remove_short_lived_systems(LPT_center_jumps, BRANCHESfb, options['min_lpt_duration_hours'])
+        #                        , latest_datetime = latest_lp_object_time - dt.timedelta(hours=options['min_lpt_duration_hours']))
 
+        lpt.lptio.lpt_systems_group_array_output_ascii('test1.txt', LPTfb, BRANCHESfb)
 
         ## Handle splitting and merging, if specified.
         if merge_split_options['allow_merge_split']:
-            LPT, BRANCHES = lpt.helpers.lpt_group_id_separate_branches(LPT_remove_short, BRANCHES_remove_short, options, verbose=True)
-            LPT, BRANCHES = lpt.helpers.lpt_split_and_merge(LPT, BRANCHES, merge_split_options, options)
+            LPT, BRANCHES = lpt.helpers.lpt_split_and_merge(LPT_remove_short, BRANCHES_remove_short, merge_split_options, options)
         else:
             LPT = LPT_remove_short.copy()
             BRANCHES = BRANCHES_remove_short.copy()
 
-        print(BRANCHES)
+        ## Re-order LPT system branches.
+        print('Re-ordering LPT branches.')
+        BRANCHES = lpt.helpers.reorder_LPT_branches(LPT, BRANCHES)
+
         ## Get "timeclusters" tracks.
         print('Calculating LPT properties.')
         if merge_split_options['allow_merge_split']:
@@ -184,7 +199,6 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
         lpt.lptio.lpt_system_tracks_output_ascii(fn_tc_base + '.txt', TIMECLUSTERS)
         lpt.lptio.lpt_systems_group_array_output_ascii(fn_tc_base + '.group_array.txt', LPT, BRANCHES)
         lpt.lptio.lpt_system_tracks_output_netcdf(fn_tc_base + '.nc', TIMECLUSTERS)
-
 
         """
         LPT Plotting
@@ -208,7 +222,8 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
 
             lpt.plotting.plot_timelon_with_lpt(ax2, dt_list, DATA_RAW['lon']
                     , timelon_rain, TIMECLUSTERS, plotting['time_lon_range']
-                    , accum_time_hours = lpo_options['accumulation_hours'])
+                    , accum_time_hours = lpo_options['accumulation_hours']
+                    , offset_time = False)
 
             ax2.set_title((dataset['label'].upper() + ' RT '
                             + '15S-15N Rain Rate and LPTs\n' + str(lpt_options['lpt_history_days']) + ' Days Ending: ' + YMDH_fancy))
@@ -222,4 +237,5 @@ def lpt_real_time_driver(dataset,plotting,output,lpo_options,lpt_options,merge_s
             file_out_base = (img_dir2 + '/lpt_time_lon_' + dataset['label'] + '_rt_' + YMDH
                                 + '__' + str(options['lpt_history_days']) + 'days')
             lpt.plotting.print_and_save(file_out_base)
+
             fig2.clf()
